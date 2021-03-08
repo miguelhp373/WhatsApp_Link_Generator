c0a002b70b54273f00b7fd65ae51e2f215e2ee3f
Miguel Henrique <miguelhp373@gmail.com>
2021-03-08 18:53:51 -0300

    Create Version 1.0.1 - 08-03-21

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import Modal from 'react-modal'
+import React, { useState } from 'react';
+import Modal from 'react-modal';
 import { FaTimes, FaLink } from 'react-icons/fa'
 
 import global from '../src/styles/global.module.css';
@@ -8,21 +8,12 @@ import container_right from '../src/styles/container-right/container.module.css'
 import ModalStyles from '../src/styles/components/modal.module.css'
 
 
-
 Modal.setAppElement('#root')
 
 
-
-
-interface FieldsInput{
-  codNumber: string;
-  phoneNumber: string;
-  messageField: string;
-}
-
-
-
 function App() {
+
+
 
   const ModalStyle = {
     overlay: {
@@ -43,24 +34,27 @@ function App() {
     }
 
   }
- 
+
 
 
   const FieldInitialValues = {
     codNumber: '',
-    phoneNumber:'',
+    phoneNumber: '',
     messageField: ''
   }
 
+
   const [modalIsOpen, setOpenModal] = useState(false);
-  const [values, setValues] = useState({FieldInitialValues})
+  const [values, setValues] = useState({ FieldInitialValues })
+  const [link, setLink] = useState('')
+  const [copy, setCopy] = useState(false)
 
 
-  function onChange(ev:React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>){
+  function getValueToField(ev: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) {
     const { id, value } = ev.currentTarget
-  
+
     localStorage.setItem(id, value)
-  
+
     setValues({
       ...values,
       [id]: value
@@ -68,37 +62,60 @@ function App() {
   }
 
   function LinkCreate() {
+    let codField = (document.getElementById('codNumber') as HTMLInputElement)
+    let phoneField = (document.getElementById('phoneNumber') as HTMLInputElement)
+    let messageField = (document.getElementById('messageField') as HTMLInputElement)
     
-    let codNumber = localStorage.getItem('codNumber')
-    let phoneNumber = localStorage.getItem('phoneNumber')
-    let message = localStorage.getItem('messageField')
-    let LinkApi = 'wa.me/';
-    let LinkGerated = `${LinkApi}55${codNumber}${phoneNumber}?text=${message}`;
-    console.log(LinkGerated)
-    localStorage.setItem('link',LinkGerated);
-    setOpenModal(true)
+    
+    
+
+    if((codField.value === '')||(phoneField.value === '')||(messageField.value === '')){
+     alert('Preencha Todos Campos Para Prosseguir!')
+    }else{
+      let codNumber = localStorage.getItem('codNumber')
+      let phoneNumber = localStorage.getItem('phoneNumber')
+      let message = localStorage.getItem('messageField')
+      let LinkApi = 'wa.me/';
+      let LinkGerated = `${LinkApi}55${codNumber}${phoneNumber}?text=${message}`;
+      localStorage.setItem('link', LinkGerated)
+      setOpenModal(true)
+      setLink(LinkGerated)
+    }
+
 
   }
+
+  function ClearFields(){
+    let cod = (document.getElementById('codNumber') as HTMLInputElement)
+    let phone = (document.getElementById('phoneNumber') as HTMLInputElement)
+    let message = (document.getElementById('messageField') as HTMLInputElement)
+    cod.value = ''
+    phone.value = ''
+    message.value = ''
+  }
+
+
+  function CopyLink() {
+    (document.getElementById('linkCopy') as HTMLInputElement).select();
+    document.execCommand('copy')
+    setCopy(true)
+  }
+
 
   function CloseModal() {
     setOpenModal(false)
+    setCopy(false)
   }
-
-/*  function CopyLink(){
-  var setLink = {(<HTMLInputElement>)document.getElementById('linkCopy').value} //parei aqui
-    
-}  */
-
 
 
   return (
-    <div className={global.container}>
 
+    <div className={global.container}>
       <div className={container_left.left_main_container}>
 
         <div className={container_left.title}>
           <div className={container_left.content}>
-            <img src="assets/whatsapp_icon.webp" alt="WhatsApp Logo" />
+            <img src="assets/logo.svg" alt="WhatsApp Logo" />
             <h1>WhatsApp Link Generator</h1>
           </div>
         </div>
@@ -109,7 +126,7 @@ function App() {
             <p>Gere Um Link Para o Seu <br />
               WhatsApp, Inserindo o Numero e <br />
               A Mensagem que pessoa enviará <br />
-              sempre que utilizar seu link
+              sempre que utilizar seu link.
             </p>
           </div>
         </div>
@@ -129,54 +146,63 @@ function App() {
           <div className={container_right.content}>
 
             <div className={container_right.field_number_phone}>
-              <input type="text" name="ddd" id="codNumber" className={container_right.field_ddd} placeholder='DDD' maxLength={2} 
-              onChange={onChange}
-              required
-             />
-              <input type="text" name="number-phone" id="phoneNumber" className={container_right.field_phone} placeholder='NÚMERO' maxLength={9} 
-              onChange={onChange}
-              required
+              <input type="text" name="ddd" id="codNumber" className={container_right.field_ddd} placeholder='DDD' maxLength={2}
+                onChange={getValueToField}
+                required
+              />
+              <input type="text" name="number-phone" id="phoneNumber" className={container_right.field_phone} placeholder='NÚMERO' maxLength={9}
+                onChange={getValueToField}
+                required
               />
             </div>
 
             <div className={container_right.field_text_area}>
-              <textarea id='messageField' className={container_right.message_field} placeholder='Mensagem' onChange={onChange} required/>
+              <textarea id='messageField' className={container_right.message_field} placeholder='Mensagem' onChange={getValueToField} required />
             </div>
 
-            <div className={container_right.button_submit_generator}>
-              <button onClick={LinkCreate} >Gerar Link</button>
+            <br />
+
+            <div className={container_right.buttonContainer}>
+              <div className={container_right.button_submit_generator}>
+                <button onClick={LinkCreate} className={container_right.btnGenerated}>Gerar Link</button>
+                <button onClick={ClearFields} className={container_right.newLink} >Novo Link</button>
 
 
 
-              <Modal
-                isOpen={modalIsOpen}
-                onRequestClose={() =>
-                  setOpenModal(false)
-                }
-                style={ModalStyle}
-              >
-                <button onClick={CloseModal} className={ModalStyles.buttonClose}><FaTimes /></button>
-                <h1 className={ModalStyles.title}>Link Gerado</h1>
+                <Modal
+                  isOpen={modalIsOpen}
+                  onRequestClose={() =>
+                    setOpenModal(false)
+                  }
+                  style={ModalStyle}
+                >
+                  <button onClick={CloseModal} className={ModalStyles.buttonClose}><FaTimes /></button>
+                  <h1 className={ModalStyles.title}>Link Gerado</h1>
                   <div className={ModalStyles.container}>
-                   <div className={ModalStyles.contentModal}>
-                    
-                    <input type="text" id='linkCopy' className={ModalStyles.linkField} />
-                   
-                    <button
-                       /* onClick={CopyLink} */
-                      className={ModalStyles.buttonLink}>
-                      <FaLink style={{ paddingTop: '2px' }} />
+                    <div className={ModalStyles.contentModal}>
+
+                      <input type="text" id='linkCopy' value={link} className={ModalStyles.linkField} />
+
+                      <button
+
+                        onClick={CopyLink}
+
+                        className={ModalStyles.buttonLink}>
+                        <FaLink style={{ paddingTop: '2px' }} />
                       &nbsp;
                       <span>
-                        Copiar Link
-                      </span>
-                    </button>
-                 
-                  </div>
-                </div>
-              </Modal>
+                          {copy ? ('Copiado') : ('Copiar Link')}
+                        </span>
+                      </button>
 
+                    </div>
+                  </div>
+                </Modal>
+
+              </div>
             </div>
+
+
           </div>
         </div>
       </div>
